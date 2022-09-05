fc7f01042ef8a7ef0a404b5c669b2e7aa61d43df
helenecs <112553299+helenecs@users.noreply.github.com>
2022-09-05 17:14:19 +0100

    Update hello.py

--- 1. Basics/hello.py
+++ 1. Basics/hello.py
@@ -1,5 +1,6 @@
 # hello.py
 # @coneill 14/09/2020
-# Prints 'Hello World'
+# Asks user for name
+# Prints a greeting including name
 name = input('Please enter your name: \n')
 print('Hello',name,'how are you today?')
